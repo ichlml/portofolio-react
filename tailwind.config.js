9a5930144a1d0d3d56579916ed0ba24b2c597a27
ichlml <ichlamal8899@gmail.com>
2024-12-02 20:01:59 +0700

    new portofolio

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html",         // Root HTML file
+    "./src/**/*.{js,ts,jsx,tsx}"], // All JS/TS/React files in the src folder],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
